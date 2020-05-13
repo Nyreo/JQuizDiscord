52a246c25136f9e449f0a942511cf283e8701aab
Joseph Mitchell <mitch137@coventry.ac.uk>
2020-05-13 20:01:38 +0100

    added basic command controller with test ping command

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
 'use strict'
 
 require('dotenv').config()
-const Discord = require("discord.js")
-const client = new Discord.Client()
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`)
+
+// fs
+const fs = require('fs')
+
+// terminal commands
+const chalk = require('chalk')
+const log = console.log
+
+// discord imports
+const {Client, MessageEmbed, Collection} = require('discord.js')
+
+// creating the client
+const bot = new Client()
+bot.commands = new Collection()
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('js'))
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`)
+
+  bot.commands.set(command.name, command)
+}
+
+log(bot.commands)
+
+// crucial ready event, once this is complete the bot will react to events
+bot.on("ready", () => {
+  console.log(`Logged in as ${chalk.blue(bot.user.tag)}!`)
 })
-client.on("message", msg => {
-  if (msg.content === "ping") {
-    msg.reply("Pong!")
+
+bot.on('message', message => {
+
+  log(`Message Received - ${message.content} from ${chalk.underline(message.author.tag)}!`)
+
+  if(bot.commands.get(message.content)) {
+    bot.commands.get(message.content).execute(message)
   }
-})
+});
 
-
-client.login(process.env.CLIENT_TOKEN)
+bot.login(process.env.CLIENT_TOKEN)
