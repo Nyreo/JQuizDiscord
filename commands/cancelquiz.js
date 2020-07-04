45bb814ed2ebe00766bc62bef14a33f4d8bf62f8
Joseph Mitchell <mitch137@coventry.ac.uk>
2020-07-04 23:18:16 +0100

    added restrictions on being able to cancel the current quiz

--- commands/cancelquiz.js
+++ commands/cancelquiz.js
@@ -5,8 +5,28 @@ module.exports = {
 	description: 'cancels the current quiz',
 	execute(message) {
 		const guild = message.guild;
+		const author = message.author;
 
-		if(quizHandler.cancelQuiz(guild.id)) return message.channel.send('The quiz has been successfully deleted.');
-		else return message.channel.send('Unable to delete the quiz.');
+		// check if the author is the host of the quiz
+		return quizHandler.fetchQuiz(guild.id)
+			.then(quiz => {
+				if(quiz) {
+					// quiz exists
+					const isHost = quiz.players[author.id].isHost;
+
+					if(isHost) {
+						// author is the host
+						quizHandler.cancelQuiz(guild.id);
+						return message.channel.send('The quiz has been successfully deleted.');
+					} else {
+						// author is not the host
+						return message.channel.send('You need to be the host to cancel a quiz.');
+					}
+				} else {
+					// quiz does not exist
+					return message.channel.send('There is currently not a quiz going on!');
+				}
+			})
+			.catch(err => message.channel.send('An error occurred'));
 	},
-}
+};
--- libs/quizHandler.js
+++ libs/quizHandler.js
@@ -25,4 +25,7 @@ module.exports = {
 			.then(() => true)
 			.catch(() => false);
 	},
+	fetchQuiz: (guildId) => {
+		return quizzes.get(guildId);
+	},
 };
